dd0b0e6d2274813a20b150cd64437412159c107d
rs-ibm <nick.freer@uk.ibm.com>
2025-04-11 12:26:49 +0100

    debug

--- docker-image-parent/scripts/create_backup_job.sh
+++ docker-image-parent/scripts/create_backup_job.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 
+cd $(dirname $0)
 get_wait_count(){
     minutes=$1
     period=$2
--- docker-image-parent/scripts/create_restore_job.sh
+++ docker-image-parent/scripts/create_restore_job.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd $(dirname $0)
 get_wait_count(){
     minutes=$1
     period=$2
