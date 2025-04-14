675404dbd30d0e5377aecef82f0baa63a74dad9e
rs-ibm <nick.freer@uk.ibm.com>
2025-04-14 13:43:47 +0100

    debug

--- docker-image-parent/scripts/create_backup_job.sh
+++ docker-image-parent/scripts/create_backup_job.sh
@@ -12,11 +12,11 @@ maxJobWait=$(get_wait_count 1 ${period}) # - 1 min
 startPodWait=$(get_wait_count 10 ${period}) # - 5 min
 
 # check there isn't a backup running already
-if [ "$(kubectl get pod --no-headers | grep db-backup-job)" != '' ] ; then
+if [ "$(kubectl get pod --no-headers | grep turbobkup)" != '' ] ; then
     echo 'ERROR: There is a backup already running, ending script'
     exit 1
 fi
-if [ "$(kubectl get pod --no-headers | grep db-restore-job)" != '' ] ; then
+if [ "$(kubectl get pod --no-headers | grep turbo-restore)" != '' ] ; then
     echo 'ERROR: There is a restore running, ending script'
     exit 1
 fi
@@ -32,7 +32,7 @@ for pvc in $(kubectl get pvc --no-headers -o custom-columns=":metadata.name" | g
 done
 
 while IFS= read -r line ; do
-    echo "$line" | tr -d '\015' | sed 's/NAME/db-backup-job/' | sed "s?COMMAND?\"/scripts/launch.sh\", \"backup.py\"?" >> dbbkup.yaml
+    echo "$line" | tr -d '\015' | sed 's/NAME/turbobkup/' | sed "s?COMMAND?\"/scripts/launch.sh\", \"backup.py\"?" >> dbbkup.yaml
     if [ "$(echo $line | grep 'volumes:')" != '' ] ; then
         cat volumes.yaml >> dbbkup.yaml
     fi
@@ -50,31 +50,31 @@ kubectl apply -f dbbkup.yaml
 # make sure the pod starts and runs
 echo '=============== wait for pod to start ==============='
 count=0
-while [ "$(kubectl get pod --no-headers | grep db-backup-job | awk '/Completed|Running/ {print $0}' )" = '' -a ${count} -lt ${startPodWait} ] ; do
+while [ "$(kubectl get pod --no-headers | grep turbobkup | awk '/Completed|Running/ {print $0}' )" = '' -a ${count} -lt ${startPodWait} ] ; do
     echo "${count}/${startPodWait}: waiting for job pod to start"
     sleep ${period}
     count=$(expr ${count} + 1)
 done
-if [ "$(kubectl get pod --no-headers | grep db-backup-job | awk '/Completed|Running/ {print $0}' )" = '' ] ; then
+if [ "$(kubectl get pod --no-headers | grep turbobkup | awk '/Completed|Running/ {print $0}' )" = '' ] ; then
     echo 'ERROR: Job Pod failed to start correctly'
     kubectl delete -f dbbkup.yaml
     failed=1
 else
     echo '=============== wait for pod to finish ==============='
     count=0
-    while [ "$(kubectl get pod --no-headers | grep Completed | grep db-backup-job | cut -f1 -d' ')" = '' -a ${count} -lt ${maxPodWait} ] ; do
+    while [ "$(kubectl get pod --no-headers | grep Completed | grep turbobkup | cut -f1 -d' ')" = '' -a ${count} -lt ${maxPodWait} ] ; do
         echo "${count}/${maxPodWait}: waiting for job pod to finish"
         sleep ${period}
         count=$(expr ${count} + 1)
     done
 
     echo '=============== Logs ==============='
-    if [ "$(kubectl get pod --no-headers | grep Completed | grep db-backup-job | cut -f1 -d' ')" = '' ] ; then
+    if [ "$(kubectl get pod --no-headers | grep Completed | grep turbobkup | cut -f1 -d' ')" = '' ] ; then
         echo 'ERROR: Pod failed to complete successfully'
         kubectl delete -f dbbkup.yaml
         failed=1
     fi
-    kubectl logs $(kubectl get pod --no-headers | grep Completed | grep db-backup-job | cut -f1 -d' ')
+    kubectl logs $(kubectl get pod --no-headers | grep Completed | grep turbobkup | cut -f1 -d' ')
     echo '============= Logs END ============='
 
 fi
--- docker-image-parent/scripts/create_restore_job.sh
+++ docker-image-parent/scripts/create_restore_job.sh
@@ -12,11 +12,11 @@ maxJobWait=$(get_wait_count 1 ${period}) # - 1 min
 startPodWait=$(get_wait_count 2 ${period}) # - 5 min
 
 # check there isn't a  restore running already
-if [ "$(kubectl get pod --no-headers | grep db-restore-job)" != '' ] ; then
+if [ "$(kubectl get pod --no-headers | grep turbo-restore)" != '' ] ; then
     echo 'ERROR: There is a restore already running, ending script'
     exit 1
 fi
-if [ "$(kubectl get pod --no-headers | grep db-backup-job)" != '' ] ; then
+if [ "$(kubectl get pod --no-headers | grep turbobkup)" != '' ] ; then
     echo 'ERROR: There is a backup running, ending script'
     exit 1
 fi
@@ -49,31 +49,31 @@ kubectl apply -f dbrestore.yaml
 # make sure the pod starts and runs
 echo '=============== wait for pod to start ==============='
 count=0
-while [ "$(kubectl get pod --no-headers | grep db-restore-job | awk '/Completed|Running/ {print $0}' )" = '' -a ${count} -lt ${startPodWait} ] ; do
+while [ "$(kubectl get pod --no-headers | grep turbo-restore | awk '/Completed|Running/ {print $0}' )" = '' -a ${count} -lt ${startPodWait} ] ; do
     echo "${count}/${startPodWait}: waiting for job pod to start"
     sleep ${period}
     count=$(expr ${count} + 1)
 done
-if [ "$(kubectl get pod --no-headers | grep db-restore-job | awk '/Completed|Running/ {print $0}' )" = '' ] ; then
+if [ "$(kubectl get pod --no-headers | grep turbo-restore | awk '/Completed|Running/ {print $0}' )" = '' ] ; then
     echo 'ERROR: Job Pod failed to start correctly'
     kubectl delete -f dbrestore.yaml
     failed=1
 else
     echo '=============== wait for pod to finish ==============='
     count=0
-    while [ "$(kubectl get pod --no-headers | grep Completed | grep db-restore-job | cut -f1 -d' ')" = '' -a ${count} -lt ${maxPodWait} ] ; do
+    while [ "$(kubectl get pod --no-headers | grep Completed | grep turbo-restore | cut -f1 -d' ')" = '' -a ${count} -lt ${maxPodWait} ] ; do
         echo "${count}/${maxPodWait}: waiting for job pod to finish"
         sleep ${period}
         count=$(expr ${count} + 1)
     done
 
     echo '=============== Logs ==============='
-    if [ "$(kubectl get pod --no-headers | grep Completed | grep db-restore-job | cut -f1 -d' ')" = '' ] ; then
+    if [ "$(kubectl get pod --no-headers | grep Completed | grep turbo-restore | cut -f1 -d' ')" = '' ] ; then
         echo 'ERROR: Pod failed to complete successfully'
         kubectl delete -f dbrestore.yaml
         failed=1
     fi
-    kubectl logs $(kubectl get pod --no-headers | grep Completed | grep db-restore-job | cut -f1 -d' ')
+    kubectl logs $(kubectl get pod --no-headers | grep Completed | grep turbo-restore | cut -f1 -d' ')
     echo '============= Logs END ============='
 fi
 
