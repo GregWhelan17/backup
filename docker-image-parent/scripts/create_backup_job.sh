e08201d43ee1677848ab296ec895856ee41de6c0
rs-ibm <nick.freer@uk.ibm.com>
2025-04-15 14:54:06 +0100

    debug

--- docker-image-parent/scripts/create_backup_job.sh
+++ docker-image-parent/scripts/create_backup_job.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ns=turbonomic
+
 get_wait_count(){
     minutes=$1
     period=$2
@@ -12,18 +14,18 @@ maxJobWait=$(get_wait_count 1 ${period}) # - 1 min
 startPodWait=$(get_wait_count 10 ${period}) # - 5 min
 
 # check there isn't a backup running already
-if [ "$(kubectl get pod --no-headers | grep turbobkup)" != '' ] ; then
+if [ "$(kubectl get pod -n ${ns} --no-headers | grep turbobkup)" != '' ] ; then
     echo 'ERROR: There is a backup already running, ending script'
     exit 1
 fi
-if [ "$(kubectl get pod --no-headers | grep turbo-restore)" != '' ] ; then
+if [ "$(kubectl get pod -n ${ns} --no-headers | grep turbo-restore)" != '' ] ; then
     echo 'ERROR: There is a restore running, ending script'
     exit 1
 fi
 
 ./scale.sh down
 
-for pvc in $(kubectl get pvc --no-headers -o custom-columns=":metadata.name" | grep -v 'timescaledb'); do
+for pvc in $(kubectl get pvc -n ${ns} --no-headers -o custom-columns=":metadata.name" | grep -v 'timescaledb'); do
     echo "        - mountPath: /pvcs/${pvc}
           name: ${pvc}" >> mounts.yaml
     echo "      - name: ${pvc}
@@ -45,36 +47,36 @@ done < job.yaml
 echo '=============== Updated YAML ==============='
 cat dbbkup.yaml
 
-kubectl apply -f dbbkup.yaml
+kubectl apply -f dbbkup.yaml -n ${ns}
 
 # make sure the pod starts and runs
 echo '=============== wait for pod to start ==============='
 count=0
-while [ "$(kubectl get pod --no-headers | grep turbobkup | awk '/Completed|Running/ {print $0}' )" = '' -a ${count} -lt ${startPodWait} ] ; do
+while [ "$(kubectl get pod -n ${ns} --no-headers | grep turbobkup | awk '/Completed|Running/ {print $0}' )" = '' -a ${count} -lt ${startPodWait} ] ; do
     echo "${count}/${startPodWait}: waiting for job pod to start"
     sleep ${period}
     count=$(expr ${count} + 1)
 done
-if [ "$(kubectl get pod --no-headers | grep turbobkup | awk '/Completed|Running/ {print $0}' )" = '' ] ; then
+if [ "$(kubectl get pod -n ${ns} --no-headers | grep turbobkup | awk '/Completed|Running/ {print $0}' )" = '' ] ; then
     echo 'ERROR: Job Pod failed to start correctly'
     kubectl delete -f dbbkup.yaml
     failed=1
 else
     echo '=============== wait for pod to finish ==============='
     count=0
-    while [ "$(kubectl get pod --no-headers | grep Completed | grep turbobkup | cut -f1 -d' ')" = '' -a ${count} -lt ${maxPodWait} ] ; do
+    while [ "$(kubectl get pod -n ${ns} --no-headers | grep Completed | grep turbobkup | cut -f1 -d' ')" = '' -a ${count} -lt ${maxPodWait} ] ; do
         echo "${count}/${maxPodWait}: waiting for job pod to finish"
         sleep ${period}
         count=$(expr ${count} + 1)
     done
 
     echo '=============== Logs ==============='
-    if [ "$(kubectl get pod --no-headers | grep Completed | grep turbobkup | cut -f1 -d' ')" = '' ] ; then
+    if [ "$(kubectl get pod -n ${ns} --no-headers | grep Completed | grep turbobkup | cut -f1 -d' ')" = '' ] ; then
         echo 'ERROR: Pod failed to complete successfully'
         kubectl delete -f dbbkup.yaml
         failed=1
     fi
-    kubectl logs $(kubectl get pod --no-headers | grep Completed | grep turbobkup | cut -f1 -d' ')
+    kubectl logs $(kubectl get pod -n ${ns} --no-headers | grep Completed | grep turbobkup | cut -f1 -d' ')
     echo '============= Logs END ============='
 
 fi
