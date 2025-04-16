44f016aafb618b10ba7049958877963c7cff4107
turbo <turbo@turbo3.cluster.local>
2025-04-16 05:30:36 -0400

    debug

--- restore-archive.sh
+++ restore-archive.sh
@@ -6,31 +6,32 @@ if [ $# -lt 1 ] ; then
 fi
 
 kubectl delete configmap archive -n turbobkup --ignore-not-found
-kubectl create configmap archive --from-literal=archive=$1 -n turbobkup
+kubectl create configmap archive --from-literal archive=$1 -n turbobkup
+kubectl get configmap archive -n turbobkup -o yaml
 kubectl apply -f testrestorejob.yaml -n turbobkup
 # make sure the pod starts and runs
 count=0
-while [ "$(kubectl get pod -n turbobkup --no-headers | grep turbo-restore | awk '/Completed|Running/ {print $0}' )" = '' -a ${count} -lt 10 ] ; do
+while [ "$(kubectl get pod -n turbobkup --no-headers | grep turborestore-parent | awk '/Completed|Running/ {print $0}' )" = '' -a ${count} -lt 10 ] ; do
     echo "${count}/10: waiting for job pod to start"
     sleep 10
     count=$(expr ${count} + 1)
 done
-if [ "$(kubectl get pod -n turbobkup --no-headers | grep turbo-restore | awk '/Completed|Running/ {print $0}' )" = '' ] ; then
+if [ "$(kubectl get pod -n turbobkup --no-headers | grep turborestore-parent | awk '/Completed|Running/ {print $0}' )" = '' ] ; then
     echo 'ERROR: Job Pod failed to start correctly'
     kubectl delete -f testrestorejob.yaml
 else
     echo '=============== wait for pod to finish ==============='
     count=0
-    while [ "$(kubectl get pod -n turbobkup --no-headers | grep Completed | grep turbo-restore | cut -f1 -d' ')" = '' -a ${count} -lt 10 ] ; do
+    while [ "$(kubectl get pod -n turbobkup --no-headers | grep Completed | grep turborestore-parent | cut -f1 -d' ')" = '' -a ${count} -lt 10 ] ; do
         echo "${count}/10: waiting for job pod to finish"
         sleep 10
         count=$(expr ${count} + 1)
     done
 
     echo '=============== Logs ==============='
-    if [ "$(kubectl get pod -n turbobkup --no-headers | grep Completed | grep turbo-restore | cut -f1 -d' ')" = '' ] ; then
+    if [ "$(kubectl get pod -n turbobkup --no-headers | grep Completed | grep turborestore-parent | cut -f1 -d' ')" = '' ] ; then
         echo 'ERROR: Pod failed to complete successfully'
         kubectl delete -f testrestorejob.yaml
     fi
 fi
-kubectl delete configmap archive -n turbobkup
+#kubectl delete configmap archive -n turbobkup
